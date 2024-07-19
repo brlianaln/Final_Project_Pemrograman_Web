e709b525462a7b7e236fa8a77e4de1527419281a
sekartajii <sekartaji@students.amikom.ac.id>
2024-07-19 17:18:59 +0700

    about

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AboutItem from "../components/AboutItem";
+import Visi from "../components/Visimisi";
+import Team from "../components/TeamItem";
+
+
+
+
+const AboutUs =()=>{
+    return(
+        <div>
+            <AboutItem/>
+            <Visi/>
+            <Team/>
+        </div>
+    )
+}
+
+export default AboutUs

6240715f1c6b2118a57bbc26fb227c27d7140124
sekartajii <sekartaji@students.amikom.ac.id>
2024-07-19 17:18:31 +0700

    index

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+import Home from './Home';
+import Footer from "../components/Footer";
+import About from './About';
+import Shop from "./Shop";
+import Cart from "./Cart";
+import Contact from "./Contact"
+
+
+import buku2 from '../components/img/buku 2.svg';
+import buku3 from '../components/img/buku3.svg';
+import buku4 from '../components/img/buku4.svg';
+import cross from '../components/img/cross.svg'; 
+
+
+
+export {Home,Navbar,Footer,buku2,buku3,buku4,cross,About,Shop,Cart,Contact}

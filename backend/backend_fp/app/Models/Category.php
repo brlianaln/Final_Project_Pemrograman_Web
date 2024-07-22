a0389f207c50a6e860cc1efe16f671c037439439
brlianaln <berlianakharismanh@students.amikom.ac.id>
2024-07-22 18:01:14 +0700

    model dan migration category

---
+++ backend/backend_fp/app/Models/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "book_id",
+        "user_id",
+        "cart_id",
+        "quantity",
+        "total_price"
+    ];
+    public function hasBook()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function hasUser()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // One-to-Many inverse with Cart
+    public function hasCart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+
+}

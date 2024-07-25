f270e4dc188f48c6ed308618a9f6371c1a635c38
atmantized <eleonoraiinez@gmail.com>
2024-07-25 14:04:14 +0700

    API

--- backend/backend_fp/routes/api.php
+++ backend/backend_fp/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CheckoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/books', [BookController::class, 'book_list']);
+Route::get('/books/search', [BookController::class, 'find_book']);
+Route::get('/books/category', [BookController::class, 'category_filter']);
+Route::get('/books/{id}', [BookController::class, 'book_detail']);
+
+Route::prefix('cart')->name('cart.')->group(function () {
+Route::get('/contents', 'CartController@getCartContents')->name('contents');
+Route::post('/add', 'CartController@addToCart')->name('add');
+Route::get('/total', 'CartController@total_price')->name('total');
+});
+
+Route::post('/checkout', [CheckoutController::class, 'checkout']);
+Route::post('/store-checkout-data', [CheckoutController::class, 'storeCheckoutData']);
+route::post('/book_list',[BookController::class, 'book_list']);
